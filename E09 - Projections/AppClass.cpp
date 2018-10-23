6e55f33ee25d5d32bff4c8f47a572c3883fdebd9
Vincent-DiCocco <vincent.dicocco@hotmail.com>
2018-10-22 21:23:30 -0400

    E09 Deliverable

--- E09 - Projections/AppClass.cpp
+++ E09 - Projections/AppClass.cpp
@@ -70,7 +70,7 @@ void Application::Display(void)
 	case 5:
 		m_pCamera->ResetCamera();
 		m_pCamera->SetPositionTargetAndUpward(AXIS_Z * -15, ZERO_V3, AXIS_Y);
-		m_pCamera->SetNearFar(vector2(4.99f, 100));
+		m_pCamera->SetNearFar(vector2(4.999999f, 100));
 		break;
 	case 6:
 		m_pCamera->ResetCamera();
